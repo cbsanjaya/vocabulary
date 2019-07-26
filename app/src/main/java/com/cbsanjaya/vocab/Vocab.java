aa6bbcf15d28709cf7bee0d97b81d4e854f1551e
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-07-26 20:17:54 +0700

    initial project

---
+++ app/src/main/java/com/cbsanjaya/vocab/Vocab.java
@@ -0,0 +1,60 @@
+package com.cbsanjaya.vocab;
+
+public class Vocab {
+    private boolean verb;
+    private String bahasa;
+    private String english;
+
+    public Vocab(boolean verb, String bahasa, String english) {
+        this.verb = verb;
+        this.bahasa = bahasa;
+        this.english = english;
+    }
+
+    public String getBahasa() {
+        return this.bahasa;
+    }
+
+    public String getEnglish1() {
+        String result="";
+
+        if (verb) {
+            result = this.english.split(";")[0];
+        } else {
+            result = this.english;
+        }
+
+        return result;
+
+    }
+
+    public String getEnglish2() {
+        String result="";
+
+        if (verb) {
+            result = this.english.split(";")[1];
+        }
+
+        return result;
+    }
+
+    public String getEnglish3() {
+        String result="";
+
+        if (verb) {
+            result = this.english.split(";")[2];
+        }
+
+        return result;
+    }
+
+    public String getEnglishIng() {
+        String result="";
+
+        if (verb) {
+            result = this.english.split(";")[3];
+        }
+
+        return result;
+    }
+}
